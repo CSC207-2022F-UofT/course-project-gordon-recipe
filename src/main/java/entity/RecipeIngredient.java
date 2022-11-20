77648081c45d43833ec1113755fa7d59f85b7526
ActuallyX <yanzhe.zheng@mail.utoronto.ca>
2022-11-20 02:50:22 -0500

    Implement ChefMode and associated tests

--- src/main/java/entity/RecipeIngredient.java
+++ src/main/java/entity/RecipeIngredient.java
@@ -29,4 +29,12 @@ public class RecipeIngredient {
     public RecipeIngredient() {
 
     }
+
+    public Ingredient getIngredient() {
+        return ingredient;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
 }
